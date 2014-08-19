f8a173215572ff91e48efa3ec420f59eba7b293b
Vu Tran <vu.tran@tikki.me>
2014-08-19 16:55:50 +0700

    s

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var queue = {};
 var host = 'http://media.movideo.us';
 var urlMod = require('url');
 var resolveUrl = urlMod.resolve;
+var _ = require('lodash');
 var md5 = function (str) {
     return crypto.createHash('md5').update(str).digest('hex');
 };
@@ -18,34 +19,33 @@ var NginxParser = require('nginxparser');
 var parser = new NginxParser('$remote_addr - $remote_user [$time_local] '
     + '"$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"');
 
-var processTS = function(urls){
+var processTS = function (urls) {
     var url = urls[0];
     console.log('url:' + url);
-    var req = request(url);
-    req.on('end', function(){
-        urls = urls.splice(0,1);
-        if(urls.length > 0) {
+    request(url, function (err, response) {
+        if (err || response.statusCode !== 200) return;
+        var urls = urls.splice(0, 1);
+        if (urls.length > 0) {
             setTimeout(function () {
                 processTS(urls);
             }, 2000);
         }
     });
-    req.on('close', function(){
-    });
 }
-var processPlaylist = function(playlistUrl) {
-    console.log('processPlaylist:' + processPlaylist);
-    request(playlistUrl, function(err, response) {
-        if (response.statusCode !== 200) return;
+var processPlaylist = function (playlistUrl) {
+    console.log('processPlaylist:' + playlistUrl);
+    request(playlistUrl, function (err, response) {
+        if (err || response.statusCode !== 200) return;
 
         var body = response.body.toString().trim();
 
-        var urls  = body.split('\n').map(function(line) {
-            if (line[0] === '#') return;
+        var urls = body.split('\n').map(function (line) {
+            if (line[0] === '#') return false;
             return resolveUrl(playlistUrl, line);
         });
-        console.log(urls);
-        processTS(urls);
+        var myurls = _.compact(urls);
+        console.log(myurls);
+        processTS(myurls);
     });
 }
 var processUrl = function (requestUrl) {
