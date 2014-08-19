58fd52374d56a8e52cd1897102a617dce1989866
Vu Tran <vu.tran@tikki.me>
2014-08-19 15:04:02 +0700

    s

--- app.js
+++ app.js
@@ -11,24 +11,31 @@ var md5 = function (str) {
 
 var server = http.createServer(function (request, response) {
     var myurl = url.parse(request.url).pathname;
-    var origurl = 'http://media.movideo.us' + myurl;
-    var mydir = path.dirname(myurl);
-    var urlmd5 = md5(mydir);
-    if(!queue[urlmd5]) {
-        queue[urlmd5] = hls(origurl, {max: 2});
-    }
+//    console.log('myurl:' + myurl);
+
+
     if (/\.m3u8$/.test(myurl)) {
+        var mydir = path.dirname(myurl);
+        var urlmd5 = md5(mydir);
+        if(!queue[urlmd5]) {
+            var origurl = 'http://media.movideo.us' + myurl;
+            queue[urlmd5] = hls(origurl, {max: 2});
+        }
+
         queue[urlmd5].playlist(function (err, pl) {
             response.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
             response.end(pl);
         });
         console.log(Object.keys(queue));
     }
-    else {
-        var stream = queue[urlmd5].segment(request.url);
-        response.setHeader('Content-Type', 'video/mp2s');
-        stream.pipe(response);
-    }
+//    else {
+//        console.log('.ts');
+//        var mydir = path.basename(myurl);
+//        var urlmd5 = mydir.split('.')[0];
+//        var stream = queue[urlmd5].segment(mydir);
+//        response.setHeader('Content-Type', 'video/mp2s');
+//        stream.pipe(response);
+//    }
 });
 
 server.listen(8080);
